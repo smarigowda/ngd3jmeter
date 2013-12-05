2866161b801d624243e852321356bd4d2c9a01ee
Santosh Marigowda <santosh.marigowda@pearson.com>
2013-12-05 17:05:27 +0000

    option to filter the plot by elapsed time or latency

--- js/myApp.js
+++ js/myApp.js
@@ -23,11 +23,16 @@ var myApp = angular.module('myApp', [])
 				// console.log($scope.tgreport.data);
 			});
 	};
+
 	// to debug
-	// window.controller_scope = $scope;
+	window.controller_scope = $scope;
+	console.log($scope);
 }])
 .controller('MyTSPlotController', ['$scope', function($scope){
 	// console.log('hello from MyTSPlotController');
+
+	window.controller_scope = $scope;
+	console.log($scope);
 }])
 .directive('durationReport', function () {
 	return {
@@ -40,6 +45,7 @@ var myApp = angular.module('myApp', [])
 			// scope of director is same as the scope of the controller MyReportController
 			// check via console, they both will have the same $id
 			window.director_scope = scope;
+			console.log(scope);
 			// console.log(elem);
 			// console.log(attrs);
 			// console.log(attrs.folderName);
@@ -57,7 +63,12 @@ var myApp = angular.module('myApp', [])
 	var directiveDefinitionObject = {
 		restrict: 'A',
 		scope: {},
+		transclude: true,
+		controller: 'MyMetricController',
 		link: function (scope, elem, attrs) {
+			window.director_scope = scope;
+			console.log(scope);
+
 			// console.log('hello from directive jmTsPlot');
 			// console.log('@ directive folder name = '.concat(attrs.folderName));
 			// console.log('@ directive file name = '.concat(attrs.fileName));
@@ -70,6 +81,16 @@ var myApp = angular.module('myApp', [])
 				var labels = _.uniq(data.map(function(d) { return d.label; } ));
 				// console.log(labels);
 
+				var metric_selected = 'elapsed';
+				var selected_label = 'ALL';
+				
+				// attach data to the scope...
+				scope.d3data = data;
+				var data_bkp = data;
+				var filt_data;
+
+
+
 				// var select = d3.select('body').append('select');
 				var select = d3.select('#'.concat(attrs.id)).append('select');
 				select.append('option').attr('value', 'ALL').text('ALL');
@@ -78,16 +99,50 @@ var myApp = angular.module('myApp', [])
 						.attr('value', function(d) { return d; })
 						.text(function(d) { return d; });
 
+				var select2 = d3.select('#'.concat(attrs.id)).append('select');
+				select2.append('option').attr('value', 'elapsed').text('elapsed');
+				select2.append('option').attr('value', 'Latency').text('Latency');
+
+				select2.on('change', function() {
+					metric_selected = this.value;
+					console.log('in d3js, metric selected = '.concat(metric_selected));
+
+					if (selected_label !== 'ALL') {
+						filt_data = _.filter(data, function(d) { return d.label === selected_label; });
+					}
+
+					// console.log('Filtered data...');
+					// console.log(filt_data);
+					// remove the plot
+					// d3.selectAll('svg').remove();
+					d3.select('#'.concat([attrs.id])).selectAll('svg').remove();
+					// console.log('data bkp......');
+					// console.log(data_bkp);
+					// re-draw the plot
+					if ( selected_label === 'ALL' ) { scope.drawPlot(data_bkp, metric_selected); } else { scope.drawPlot(filt_data, metric_selected); }
+
+				})
+
 				// console.log('@ directive: select is....');
 				// console.log(select);
+				// add another selection for metric
+				// scope.$apply( d3.select('#'.concat(attrs.id))
+				// 						.append('select')
+				// 						.attr('id', 'metric')
+				// 						.attr('ng-model', 'metric.name'));
+				// d3.selectAll('#metric').append('option').attr('value', 'elapsedTime').text('elapsedTime');
+				// //selectMetric.append('option').attr('value', 'latency').text('latency');
 
-				var data_bkp = data;
-				var filt_data;
+				// scope.$apply(d3.select('body').append('div').text('hello...div {{ metric.name }}'));
+
+				//scope.$apply(selectMetric);
+
 
 					// filter the data and redraw the plot
 						select.on('change', function() {
-
-							var selected_label = this.value;
+							console.log(scope.getMetric());
+							console.log('in label select, metric selected = '.concat(metric_selected));
+							selected_label = this.value;
 
 							// console.log('selected value:'.concat(this.value));
 
@@ -103,14 +158,18 @@ var myApp = angular.module('myApp', [])
 							// console.log('data bkp......');
 							// console.log(data_bkp);
 							// re-draw the plot
-							if ( selected_label === 'ALL' ) { scope.drawPlot(data_bkp); } else { scope.drawPlot(filt_data); }
+							if ( selected_label === 'ALL' ) { scope.drawPlot(data_bkp, metric_selected); } else { scope.drawPlot(filt_data, metric_selected); }
+
 						});
 				// console.log('csv data read:');
-				scope.drawPlot(data_bkp);
+				scope.drawPlot(data_bkp, metric_selected);
 
 			});
+			
 
-			scope.drawPlot = function(data) {
+			scope.drawPlot = function(data, ydataLabel) {
+
+					console.log('ydataLabel = '.concat(ydataLabel));
 
 					var margin = {top: 30, right: 20, bottom: 50, left: 50},
 						width = 1000 - margin.left - margin.right,
@@ -130,7 +189,8 @@ var myApp = angular.module('myApp', [])
 
 					// convert time stamp to number
 					data.map(function(d) { d.timeStamp = +d.timeStamp; });
-					data.map(function(d) { d.elapsed = +d.elapsed; });
+					// data.map(function(d) { d.elapsed = +d.elapsed; });
+					data.map(function(d) { d[ydataLabel] = +d[ydataLabel]; });
 
 					// console.log(data);
 					x.domain(d3.extent(data.map(function(d) {return d.timeStamp;})));
@@ -141,7 +201,7 @@ var myApp = angular.module('myApp', [])
 					// console.log(x.range());
 					// console.log(x(new Date(0)));
 					// set y domain
-					y.domain(d3.extent(data.map(function(d){return d.elapsed;})));
+					y.domain(d3.extent(data.map(function(d){return d[ydataLabel];})));
 					// console.log(y.domain());
 					var svg = d3.select('#'.concat(attrs.id)).append("svg")
 									.attr("width", width + margin.left + margin.right)
@@ -169,12 +229,12 @@ var myApp = angular.module('myApp', [])
 					svg.selectAll("scatter-dots")
 						.data(data)
 						.enter().append("svg:circle")
-						.attr("cy", function (d) { return y(d.elapsed); } )
+						.attr("cy", function (d) { return y(d[ydataLabel]); } )
 						.attr("cx", function (d) { return x(d.timeStamp); } )
 						.attr("r", 5)
 						.attr('fill', function(d) { if (d.elapsed > 5000) { return 'red'; } else { return 'green'; } })
 						.style("opacity", 0.3)
-						.append("title").text(function(d){ return ''.concat(d.elapsed, ':' , d.label); });
+						.append("title").text(function(d){ return ''.concat(ydataLabel, " : ", d[ydataLabel], ' : ' , d.label); });
 			};
 
 		},
@@ -184,4 +244,22 @@ var myApp = angular.module('myApp', [])
 	// so that we can see all the variables at this point
 	// debugger; 
 	return directiveDefinitionObject;
-});
+})
+.controller('MyMetricController', ['$scope', function($scope){
+	// console.log('hello from MyTSPlotController');
+	window.MyMetricController_scope = $scope;
+	console.log($scope);
+
+	$scope.metric = { name: 'DUMMY'};
+
+	$scope.passMetric = function(metric_name) {
+		console.log('metric passed = '.concat(metric_name));
+		$scope.metric.name = metric_name;
+		console.log($scope.metric.name);
+	};
+
+	$scope.getMetric = function() {
+		return $scope.metric;
+	};
+
+}]);
