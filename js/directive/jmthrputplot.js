e5d0b71d2f2c1c4374e00ff94162a84d6e4fc215
Santosh Marigowda <santosh.marigowda@pearson.com>
2014-01-09 09:30:44 +0000

    sync

--- js/directive/jmthrputplot.js
+++ js/directive/jmthrputplot.js
@@ -1,5 +1,5 @@
 myApp
-.directive('jmThroughputPlot', ['metriclist', function(metriclist) {
+.directive('jmThroughputPlot', ['metriclist', '$window', function(metriclist, window) {
 	var directiveDefinitionObject = {
 		restrict: 'A',
 		scope: {
@@ -10,6 +10,8 @@ myApp
 		controller: 'myd3Controller',
 		link: function (scope, elem, attrs) {
 				scope.getData(scope.fileName, scope.folderName).then(function(data) {
+
+					window.rawdata = data;
 					// use unique data labels
 					var labels = _.uniq(data.map(function(d) { return d.label; } ));
 
@@ -23,7 +25,7 @@ myApp
 
 					// for each unique timeStamp2, count samples
 					var groupbyTimeStamp = d3.nest()
-							.key(function(d) { return d.timeStamp; })
+							.key(function(d) { return d.timeStamp2; })
 							.sortKeys(d3.ascending)
 							// for each group of label perform the below group wise computation
 							.rollup(function(d) {
@@ -31,74 +33,17 @@ myApp
 							})
 							.entries(data);
 
-					// defaults
-					var metric_selected = 'elapsed';
-					var selected_label = 'ALL';
-				
-					var data_bkp = data;
-					var filt_data;
-
-					// construct an option to select 'Transaction'
-					var select = d3.select('#'.concat(attrs.id)).append('select');
-					select.append('option').attr('value', 'ALL').text('ALL');
-					select.selectAll('option')
-							.data(labels).enter().append('option')
-							.attr('value', function(d) { return d; })
-							.text(function(d) { return d; });
-
-					// construct an option to select a metric 
-					// (elapsed or Latency)
-					var select2 = d3.select('#'.concat(attrs.id)).append('select');
-					select2.selectAll('option')
-							.data(metriclist.names).enter().append('option')
-							.attr('value', function(d) { return d; })
-							.text(function(d) { return d; });
-
-					select2.on('change', function() {
-						metric_selected = this.value;
-
-						d3.select('#'.concat([attrs.id])).selectAll('svg').remove();
-
-						if ( selected_label === 'ALL' ) {
-							scope.drawPlot(data_bkp, metric_selected);
-						} else {
-							filt_data = _.filter(data, function(d) { return d.label === selected_label; });
-							scope.drawPlot(filt_data, metric_selected);
-						}
-
-					});
-
-					// filter the data and redraw the plot
-					select.on('change', function() {
-						// console.log('in label select, metric selected = '.concat(metric_selected));
-						selected_label = this.value;
-						// console.log('selected value:'.concat(this.value));
-						if (selected_label !== 'ALL') {
-							filt_data = _.filter(data, function(d) { return d.label === selected_label; });
-						}
-						// console.log('Filtered data...');
-						// console.log(filt_data);
-						// remove the plot
-						// d3.selectAll('svg').remove();
-						d3.select('#'.concat([attrs.id])).selectAll('svg').remove();
-						// console.log('data bkp......');
-						// console.log(data_bkp);
-						// re-draw the plot
-						if ( selected_label === 'ALL' ) {
-							scope.drawPlot(data_bkp, metric_selected);
-						} else {
-							scope.drawPlot(filt_data, metric_selected);
-						}
-					});
-
-					// draw first time.
-					scope.drawPlot(data_bkp, metric_selected);
+					console.log(window);
+					window.groupbyTimeStamp = groupbyTimeStamp;
+					scope.drawPlot(groupbyTimeStamp);
 
 			});
 
-			scope.drawPlot = function(data, ydataLabel) {
+			scope.drawPlot = function(data) {
 
-					// console.log('ydataLabel = '.concat(ydataLabel));
+					var div = d3.select('#'.concat(attrs.id)).append("div")
+									.attr("class", "tooltip")
+									.style("opacity", 0);
 
 					var margin = {top: 30, right: 20, bottom: 80, left: 50},
 						width = 1000 - margin.left - margin.right,
@@ -117,16 +62,20 @@ myApp
 						.orient("left");
 
 					// convert time stamp to number
-					data.map(function(d) { d.timeStamp = +d.timeStamp; });
-					// convert the value of elapsed or Latency to number
-					data.map(function(d) { d[ydataLabel] = +d[ydataLabel]; });
+					data.map(function(d) { d.key = +d.key; });
 
 					// console.log(data);
-					x.domain(d3.extent(data.map(function(d) {return d.timeStamp;})));
+					// x.domain(d3.extent(data.map(function(d) { return d.key; })));
+					x.domain([d3.min(data.map(function(d) { return d.key; })),
+						d3.max(data.map(function(d) { return d.key; }))]);
 
-					// console.log(x(new Date(0)));
 					// set y domain
-					y.domain(d3.extent(data.map(function(d){return d[ydataLabel];})));
+					// y.domain(d3.extent(data.map(function(d){ return d.values.throughput; })));
+					y.domain([d3.min(data.map(function(d){ return d.values.throughput; })),
+						d3.max(data.map(function(d){ return d.values.throughput; }))]);
+					
+					// debugger;
+
 					// console.log(y.domain());
 					var svg = d3.select('#'.concat(attrs.id)).append("svg")
 									.attr("width", width + margin.left + margin.right)
@@ -157,12 +106,29 @@ myApp
 					svg.selectAll("scatter-dots")
 						.data(data)
 						.enter().append("svg:circle")
-						.attr("cy", function (d) { return y(d[ydataLabel]); } )
-						.attr("cx", function (d) { return x(d.timeStamp); } )
-						.attr("r", 5)
-						.attr('fill', function(d) { if (d[ydataLabel] > 1000) { return 'red'; } else { return 'green'; } })
-						.style("opacity", 0.3)
-						.append("title").text(function(d){ return ''.concat(ydataLabel, " : ", d[ydataLabel], ' : ' , d.label); });
+						.attr("cy", function (d) { return y(d.values.throughput); } )
+						.attr("cx", function (d) { return x(d.key); } )
+						.attr("r", 2)
+						.on("mouseover", function(d, i) {
+							// console.log(d3.mouse(this));
+							// console.log(d3.event);
+							// console.log(d3.event.timeStamp);
+							// console.log(d);
+							// console.log(i);
+							// console.log({"x": d3.event.x, "y": d3.event.y});
+							div.transition()
+								.duration(0)
+								.style("opacity", 1.0);
+
+							div.html(d.values.throughput)
+								.style("left", (d3.event.pageX) + "px")
+								.style("top", (d3.event.pageY - 30) + "px");
+						})
+						.on("mouseout", function(d) {
+							div.transition()
+								.duration(2000)
+								.style("opacity", 0);
+						});
 			};
 
 		}
